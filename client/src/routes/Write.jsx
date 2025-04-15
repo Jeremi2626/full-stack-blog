8ec5f5adf145837de5f390cba1aa5512e59ea072
Jeremi Rojas <Jeremirojas2626@gmail.com>
2025-04-15 00:38:05 -0400

    Image/Video Upload

--- client/src/routes/Write.jsx
+++ client/src/routes/Write.jsx
@@ -1,17 +1,32 @@
-import { useAuth, useUser } from "@clerk/clerk-react"
+import { useAuth, useUser } from "@clerk/clerk-react";
 import 'react-quill-new/dist/quill.snow.css';
 import ReactQuill from 'react-quill-new';
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react"; // Agregamos useEffect aquí
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import Upload from "../components/Upload";
 
 const Write = () => {
   const {isLoaded, isSignedIn} = useUser()
   const [value, setValue] = useState('');
+  const [cover, setCover] = useState();
+  const [img, setImg] = useState();
+  const [video, setVideo] = useState();
+  const [progress, setProgress] = useState(0);
 
+  useEffect(() => {
+    img && setValue((prev) => prev + `<p><image src="${img.url}"/></p>`);
+  }, [img]);
 
+  useEffect(() => {
+    video &&
+      setValue(
+        (prev) => prev + `<p><iframe class="ql-video" src="${video.url}"/></p>`
+      );
+  }, [video]);
+ 
   const navigate = useNavigate()
 
   const {getToken} = useAuth()
@@ -46,6 +61,7 @@ const Write = () => {
     const formData = new FormData(e.target);
     
     const data = {
+      //img: cover.Path || "",
       title: formData.get('title'),
       category: formData.get('category'),
       desc: formData.get('desc'),
@@ -62,7 +78,11 @@ const Write = () => {
     <div className='h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6'>
       <h1 className="text-xl font-light">Create a New Post</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-6 flex-1 mb-6" action="">
-        <button className="w-max p-2 shadow=md rounded-xl text-sm text-gray-500 bg-white ">Add a cover image</button>
+        <Upload type="image" setProgress={setProgress} setData={setCover}>
+          <button className="w-max p-2 shadow=md rounded-xl text-sm text-gray-500 bg-white ">
+              Add a cover image
+          </button>
+        </Upload>
         <input 
           className="text-4xl font-semibold bg-transparent outline-none" 
           type="text" 
@@ -84,19 +104,36 @@ const Write = () => {
             <option value="marketing">Marketing</option>
           </select>
         </div>
-        <textarea className=" p-4 rounded-xl bg-white shadow-md" name="desc" placeholder="Write your story here" />
-        <ReactQuill 
-          theme="snow" 
-          className="flex-1  rounded-xl bg-white shadow-md"
-          value={value} 
-          onChange={setValue}
+        <textarea 
+          className=" p-4 rounded-xl bg-white shadow-md" 
+          name="desc" 
+          placeholder="Write your story here" 
+        />
+        <div className="flex flex-1 ">
+          <div className="flex flex-col gap-2 mr-2">
+            <Upload type="image" setProgress={setProgress} setData={setImg}>
+              🌆
+            </Upload>
+            <Upload type="video" setProgress={setProgress} setData={setVideo}>
+              ▶️
+            </Upload>
+          </div>
+          <ReactQuill
+            theme="snow"
+            className="flex-1 rounded-xl bg-white shadow-md"
+            value={value}
+            onChange={setValue}
+            readOnly={0 < progress && progress < 100}
           />
-        <button 
-          disabled={mutation.isPending}
-          className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 disabled:bg-gray-400 disabled:cursor-not-allowed">
-          {mutation.isPending ? "Loaging..." :"Send"}
+        </div>
+        <button
+          disabled={mutation.isPending || (0 < progress && progress < 100)}
+          className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 disabled:bg-blue-400 disabled:cursor-not-allowed"
+        >
+          {mutation.isPending ? "Loading..." : "Send"}
         </button>
-        {mutation.isError && <span>{mutation.error.message}</span> }
+        {"Progress:" + progress}
+        {/* {mutation.isError && <span>{mutation.error.message}</span>} */}
       </form>
     </div>
   );
