64b07cea57adf8603a3dd24a8e10e386d653ee6b
Jeremi Rojas <Jeremirojas2626@gmail.com>
2025-04-23 14:28:00 -0400

    Proyecto terminado

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -4,17 +4,20 @@ import {
   getPost, 
   createPost,
   deletePost, 
-  uploadAuth
+  uploadAuth,
+  featurePost
 } from '../controllers/post.controller.js'; // Importar los controladores de los posts
+import increaseVisit from '../middlewares/increaseVisit.js';
 
 const router = express.Router();
 
 router.get("/upload-auth", uploadAuth); // Obtener la autenticación para la subida de archivos
 
 router.get("/", getPosts);      // Obtener todos los posts
-router.get("/:slug", getPost);      // Obtener un post por su slug
+router.get("/:slug", increaseVisit, getPost);      // Obtener un post por su slug
 router.post("/", createPost);         // Crear un nuevo post
 router.delete("/:id", deletePost);      // Eliminar un post por su ID
+router.patch("/feature", featurePost); // Destacar un post
   
 
 
--- client/src/components/PostListItem.jsx
+++ client/src/components/PostListItem.jsx
@@ -1,34 +1,36 @@
-import { Link } from "react-router-dom"
-import  Image  from "./Image";
+import { Link } from "react-router-dom";
+import Image from "./Image";
+import { format } from "timeago.js";
 
+const PostListItem = ({ post }) => {
 
-
-const PostListItem = () => {
   return (
-    <div className='flex flex-col xl:flex-row gap-8'>
-        {/* Image */}
+    <div className="flex flex-col xl:flex-row gap-8 mb-12">
+      {/* image */}
+      {post.img && (
         <div className="md:hidden xl:block xl:w-1/3">
-            <Image src="postImg.jpeg" className="rounded-2xl object-cover" w="735"/>
+          <Image src={post.img} className="rounded-2xl object-cover" w="735" />
         </div>
-        {/* details */}
-        <div className="flex flex-col gap-4 xl:w-2/3">
-            <Link to="/test" className="text-4xl font-semibold">
-                Post Title Here 
-            </Link>
-            <div className="flex items-center gap-2 text-gray-400 text-sm">
-                <span>Written by</span>
-                <Link className="text-blue-800" >Author Name</Link>
-                <span>on</span>
-                <Link className="text-blue-800" >Wed Design</Link>
-                <span>2 days ago</span>
-            </div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
-            </p>
-            <Link to="/test" className="underline text-blue-800 text-sm">Read More</Link>
+      )}
+      {/* details */}
+      <div className="flex flex-col gap-4 xl:w-2/3">
+        <Link to={`/${post.slug}`} className="text-4xl font-semibold">
+          {post.title}
+        </Link>
+        <div className="flex items-center gap-2 text-gray-400 text-sm">
+          <span>Written by</span>
+          <Link className="text-blue-800" to={`/posts?author=${post.user.username}`}>{post.user.username}</Link>
+          <span>on</span>
+          <Link className="text-blue-800">{post.category}</Link>
+          <span>{format(post.createdAt)}</span>
         </div>
+        <p>{post.desc}</p>
+        <Link to={`/${post.slug}`} className="underline text-blue-800 text-sm">
+          Read More
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostListItem
+export default PostListItem;
