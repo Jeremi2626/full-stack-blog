aa726dd82401f2b64ed30bda88551591a58fed5f
Jeremi Rojas <Jeremirojas2626@gmail.com>
2025-04-23 17:45:39 -0400

    Produccion

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://jeremi2626.github.io/full-stack-blog/', // Cambia esto a la ruta de tu proyecto
 })
